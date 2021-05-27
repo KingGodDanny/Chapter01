36b40d17a446525aa7ce4e125c16fb18d640c309
Danny <ctct25@naver.com>
2021-05-27 17:48:58 +0900

    21/05/27 반복문 수업내용합산

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,19 @@
+package com.javaex.ex04;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		
+		while (i<5) { // 1<5(t), 2<5(t), 3<5(t), 4<5(t), 5<5(t), 1<6(t)
+			System.out.println("I Like java" + i);
+			
+			i++;  //i=1+i   ++i
+			
+		}
+
+	}
+
+}
